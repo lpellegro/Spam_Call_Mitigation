8bef910f272cdc7e3a467774ac23f598050778d4
Luca Pellegrini <88320330+lpellegro@users.noreply.github.com>
2021-08-02 17:43:04 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -47,15 +47,15 @@ credentials = {'expe_cluster1':'expe.example.com',
 
 Example for two clusters
 
-credentials = {'expe_cluster1':'expe1a.example.com', #FQDN of cluster1 primary peer
-               'url_cluster1':'https://expe1a.example.com', #URL for cluster1 (usually to the primary peer); the URL should include the port if different from 443 (see below)
-               'expe1a.example.com':['admin', 'password1a'], #username and password for all peers in all clusters (see below)
+credentials = {'expe_cluster1':'expe1a.example.com', 
+               'url_cluster1':'https://expe1a.example.com',
+               'expe1a.example.com':['admin', 'password1a'], 
                'expe1b.example.com':['admin', 'password1b'],  
-               '192.0.2.201':'expe1a.example.com', #IP addresses for all peers in all clusters
+               '192.0.2.201':'expe1a.example.com', 
                '192.0.2.202':'expe1b.example.com', 
 
-               'expe_cluster2':'expe2a.example.com', #FQDN of cluster2 primary peer; add more lines if more clusters are involved
-               'url_cluster2':'https://expe2a.example.com:7443', #URL for cluster2 (includes port 7443 as admin HTTPS traffic uses this port)
+               'expe_cluster2':'expe2a.example.com', 
+               'url_cluster2':'https://expe2a.example.com:7443', 
                'expe2a.example.com':['admin', 'password2a'], 
                'expe2b.example.com':['admin', 'password2b'], 
                'expe2c.example.com':['admin', 'password2c'],
@@ -63,8 +63,8 @@ credentials = {'expe_cluster1':'expe1a.example.com', #FQDN of cluster1 primary p
                '203.0.113.202':'expe2b.example.com',
                '203.0.113.203':'expe2c.example.com',
 
-               'roomID':'abcdefghilmnopqrstuvz0123456789abcdefghilmnopqrstuvz0123456789abcdefghilmnopqrstuvz0123456789', #Webex room ID to send and receive notifications
-               'bearer':'MNBVCXZLKJGHGFDSAPOIUYTREWQMNBVCXZLKJGHGFDSAPOIUYTREWQMNBVCXZLKJGHGFDSAPOIUYTREWQ-09876-54321-012345678', #Webex Bot Bearer 
+               'roomID':'abcdefghilmnopqrstuvz0123456789abcdefghilmnopqrstuvz0123456789abcdefghilmnopqrstuvz0123456789', 
+               'bearer':'MNBVCXZLKJGHGFDSAPOIUYTREWQMNBVCXZLKJGHGFDSAPOIUYTREWQMNBVCXZLKJGHGFDSAPOIUYTREWQ-09876-54321-012345678', 
                'apikey':'ab_CDEFGHILMNOPQRST0123', #API key to perform IP address lookup on https://www.whoisxmlapi.com
 
                'jailed_file':'/var/www/html/jailedIP.txt', 
