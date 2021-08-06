786b700edb0d30bc59c7f14c925ee7ccdc37a347
Luca Pellegrini <88320330+lpellegro@users.noreply.github.com>
2021-08-06 10:47:10 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -6,25 +6,6 @@ Created on Fri Jun 25 16:39:35 2021
 @author: lpellegr
 """
 
-#def credentials():
-credentials = {'expe_cluster1':'us-expe1.ent-pa.com', 
-               'expe_cluster2':'us-expe2.ent-pa.com',
-               'url_cluster1':'https://us-expe1.ent-pa.com',
-               'url_cluster2':'https://us-expe2.ent-pa.com',
-               'us-expe1.ent-pa.com':['admin', 'PA1s3KurP@swd!'], 
-               'us-expe2.ent-pa.com':['admin', 'PA1s3KurP@swd!'], 
-               'us-expe3.ent-pa.com':['admin', 'PA1s3KurP@swd!'], 
-               'roomID':'Y2lzY29zcGFyazovL3VzL1JPT00vNTEzYjU5YTAtZDI5Yi0xMWViLTg1OWYtZWY2Yzc1YWU4Y2Qw',
-               'bearer':'NTFhZDZjMGUtNDY4Zi00MTM4LTk0NmMtNDQxNzc5ZTkyMzUxYzJmOWI3YTgtYjQ5_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f',
-               'apikey':'at_zG1V6kwfPhkxYk57qc2MnC3ldydtc',
-               'jailed_file':'/var/www/html/jailedIP.txt', 
-               'exempt_file':'/var/www/html/exemptions.txt', 
-               'state_machine':'/var/www/html/card_status.csv',
-               '173.36.246.231':'us-expe1.ent-pa.com', 
-               '173.36.246.232':'us-expe2.ent-pa.com', 
-               '173.36.246.234':'us-expe3.ent-pa.com',
-                'activity_list_path':'/var/www/html/'}
-
 
 
 
